e3664c3760e53e9dd96c81a3d064421f9ec2322e
ericlecoutre <eric.lecoutre@welovedatascience.com>
2025-03-05 12:16:33 +0000

    doc updated to pass tests

--- R/00_onLoad.R
+++ R/00_onLoad.R
@@ -8,12 +8,7 @@
     targeter.use_naming_conventions = FALSE,
     targeter.useNA = "ifany",
     targeter.author = "welovedatascience targeter package",
-    targeter.auto_install_deps = TRUE,
-    targeter.brandfile =file.path(
-      find.package("targeter", lib.loc = .libPaths()),
-      "ressources",
-      "_brand.yml"
-    )
+    targeter.auto_install_deps = TRUE
   )
   toset <- !(names(op.targeter) %in% names(op))
   if (any(toset)) options(op.targeter[toset])
